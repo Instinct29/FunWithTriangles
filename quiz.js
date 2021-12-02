596c5f40659a8e74b8cdec1576e950794949de1c
Instinct29 <89262248+Instinct29@users.noreply.github.com>
2021-12-02 09:37:07 +0530

    done
    done with styling as well as with logic including correction for negative values too--
    ALL GUCCIII MAAAH MAN

--- quiz.js
+++ quiz.js
@@ -1,21 +1,21 @@
-const quizForm = document.querySelector(".questions");
+const quizForm = document.querySelector(".questionss");
 const submitbutton = document.querySelector("#submit-btn");
 const output2 = document.querySelector("#output-text");
 const ans = ["90°","Right angled","obtuse triangle","acute triangle"];
 
-submitbutton.addEventListener('click',checkans);
+submitbutton.addEventListener('click',clickHandler);
+function clickHandler(){
+    console.log("clicked");
+    let score1 = 0;
+    let index1 = 0;
 
-function checkans(){
-    let score = 0;
-    let index = 0;
-    let formResults = new FormData(quizForm);
+    const correctAns = new FormData(quizForm);
 
-    for(let value of formResults.values()){
-        if( value === ans[index]){
-            score++;
+    for(let value of correctAns.values()){
+        if(value === ans[index1]){
+            score1++;
         }
-        index++;
+        index1++;
     }
-    output2.innerText = "Your score is"+" "+ score;
-  
+    output2.textContent = "Your score is  " + score1;
 }
